a592adf93dadb76ce1d6ba22957111c720777312
geun <taeguen714@naver.com>
2020-07-03 14:34:17 +0900

    Initial commit

---
+++ app/src/main/java/com/geun/ex93github/MainActivity.java
@@ -0,0 +1,14 @@
+package com.geun.ex93github;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
